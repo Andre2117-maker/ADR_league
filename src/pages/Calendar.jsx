d55fd4e5849f7f12aafd2d8c275bee66d5c29e95
Andre2117-maker <andrehenriq2005@gmail.com>
2026-03-03 16:49:40 -0400

    fix: match case sensitivity for calendar css import

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import html2canvas from "html2canvas";
 import { useNavigate } from "react-router-dom";
-import "../styles/Calendar.css";
+import "../styles/calendar.css";
 
 function Calendar({
   matches,
