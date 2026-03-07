1886d9f4e12d756f7e5d656761ab30b36f68246f
Andre2117-maker <andrehenriq2005@gmail.com>
2026-03-07 00:18:01 -0400

    ajeitei o scroll da legenda

--- src/components/RankingTable.jsx
+++ src/components/RankingTable.jsx
@@ -142,21 +142,21 @@ export default function RankingTable({
             })}
           </tbody>
         </table>
-        <div className="legend-container">
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#007bff" }}
-            ></span>
-            <span>Prêmios</span>
-          </div>
-          <div className="legend-item">
-            <span
-              className="legend-color"
-              style={{ backgroundColor: "#dc3545" }}
-            ></span>
-            <span>Punição</span>
-          </div>
+      </div>
+      <div className="legend-container">
+        <div className="legend-item">
+          <span
+            className="legend-color"
+            style={{ backgroundColor: "#007bff" }}
+          ></span>
+          <span>Prêmios</span>
+        </div>
+        <div className="legend-item">
+          <span
+            className="legend-color"
+            style={{ backgroundColor: "#dc3545" }}
+          ></span>
+          <span>Punição</span>
         </div>
       </div>
     </main>
