2a25af88ca2fa23047f106e29d6a3b3149303016
Andre2117-maker <andrehenriq2005@gmail.com>
2026-03-27 00:01:05 -0400

    confete

--- src/components/Easter Egg/Altar.jsx
+++ src/components/Easter Egg/Altar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import confetti from "canvas-confetti";
 import "./altar.css";
 
-// Imports corretos conforme sua pasta
+// Imports das fotos
 import carlos1 from "../../assets/easteregg/carlos1.jpeg";
 import carlos2 from "../../assets/easteregg/carlos2.jpeg";
 import carlos3 from "../../assets/easteregg/carlos3.png";
@@ -10,18 +11,18 @@ import carlos3 from "../../assets/easteregg/carlos3.png";
 const Altar = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    // Confete ao entrar no Santuário
+    confetti({
+      particleCount: 150,
+      spread: 70,
+      origin: { y: 0.6 },
+      colors: ["#d4af37", "#ffffff", "#aa8920"],
+    });
+  }, []);
+
   return (
     <div className="altar-container">
-      {/* Eye of the Tiger - YouTube ID: btPJPFnesVw */}
-      <iframe
-        width="0"
-        height="0"
-        src="https://www.youtube.com/embed/btPJPFnesVw?autoplay=1"
-        frameBorder="0"
-        allow="autoplay"
-        title="Eye of the Tiger"
-      ></iframe>
-
       <div className="altar-content">
         <p className="dedication-text">
           PÁGINA DEDICADA AO NOSSO QUERIDO AMIGO CARLOS
@@ -29,7 +30,7 @@ const Altar = () => {
 
         <h1 className="glow-text">O MITO</h1>
 
-        {/* FOTO PRINCIPAL (carlos1) */}
+        {/* FOTO PRINCIPAL COM AURA */}
         <div className="aura-wrapper">
           <div className="aura-effect"></div>
           <div className="altar-frame">
@@ -41,7 +42,7 @@ const Altar = () => {
           </div>
         </div>
 
-        {/* GALERIA (carlos2 e carlos3) */}
+        {/* GALERIA DOS TÍTULOS */}
         <div className="carlos-gallery">
           <div className="gallery-card">
             <div className="gallery-item">
@@ -58,16 +59,15 @@ const Altar = () => {
           </div>
         </div>
 
+        {/* BOX DE CONQUISTAS */}
         <div className="stats-container">
           <div className="stats-box">
             <h2>FEITOS LENDÁRIOS</h2>
             <ul>
-              <li>Carlos deu um soco em Gustavo por questão de lugar</li>
-              <li>Carlos mora atualmente na argentina</li>
-              <li>
-                Carlos precisou de 14 pontos pra passar em certas matérias
-              </li>
-              <li>Carlos atualmente joga pelo time de easport do adr</li>
+              <li>Soco em Gustavo por questão de lugar</li>
+              <li>Mora atualmente na Argentina</li>
+              <li>Precisou de 14 pontos para passar</li>
+              <li>Joga pelo time de e-sports do ADR</li>
             </ul>
           </div>
 
