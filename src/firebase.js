ae09291822eaf456795d722ac4e27ff1441353a0
Andre2117-maker <andrehenriq2005@gmail.com>
2026-05-10 16:54:41 -0400

    privando o firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmNaQxA3Mv54N4U6KJ5tA8wt0traP6yaA",
-  authDomain: "adr-league.firebaseapp.com",
-  projectId: "adr-league",
-  storageBucket: "adr-league.firebasestorage.app",
-  messagingSenderId: "569066041468",
-  appId: "1:569066041468:web:d7f4c256078c78de44d9f9",
-  measurementId: "G-C7Y80FGV2R",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
